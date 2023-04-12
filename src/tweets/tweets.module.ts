41b677776ceca46b061a30986ca4d3a7cc8a4680
Samuel Vieira <goodluff@hotmail.com>
2023-04-12 00:18:35 -0300

    Adicionando arquivos iniciais do projeto

---
+++ src/tweets/tweets.module.ts
@@ -0,0 +1,12 @@
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Module } from '@nestjs/common';
+import { TweetsService } from './tweets.service';
+import { TweetsController } from './tweets.controller';
+import { Tweet } from './entities/tweet.entity';
+
+@Module({
+  imports: [SequelizeModule.forFeature([Tweet])],
+  controllers: [TweetsController],
+  providers: [TweetsService],
+})
+export class TweetsModule {}
